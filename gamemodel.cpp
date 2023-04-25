c485d4881a0fd5b987fcf434dfed226108d74770
DragonJousting <tankeremail@gmail.com>
2023-04-24 18:17:14 -0600

    fixed conveyor

--- gamemodel.cpp
+++ gamemodel.cpp
@@ -172,7 +172,7 @@ void GameModel::updateWorld() {
         emit worldInit();
     }
 
-    conveyorPosition += conveyorSpeed;
+    conveyorPosition += conveyorSpeed/60;
     conveyorPosition = conveyorPosition%CONVEYOR_DISTANCE;
     // It is generally best to keep the time step and iterations fixed.
     if (isholdingfish) {
